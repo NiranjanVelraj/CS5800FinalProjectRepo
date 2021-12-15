cb00a7257e3e15f8304f270f60eaa590d8825ebc
NiranjanVelraj <niranjanvelraj@gmail.com>
2021-12-15 15:08:04 -0500

    Changed Repo Structure and implemented 2Trip for demo

--- TwoTripJS/2Trip.js
+++ TwoTripJS/2Trip.js
@@ -1,19 +1,15 @@
 let blossom = require("./AdditionalFiles/edmonds-blossom");
 const MAX_DISTANCE = 10000;
 const FOOD_BANK_POS = [0, 0];
-const REST_POS = [
-  [1.5, 4.5],
-  [3.3, 3.1],
-  [3.5, 1.0],
-  [2.5, -1.0],
-  [3.2, -3.0],
-  [2.0, -4.0],
-  [0.8, -3.0],
-  [-1.5, -4.0],
-  [-3.5, -2.0],
-  [-4.0, 1.0],
-  [-1.8, 1.5],
-  [-2.5, 3.5],
+
+const distFromStart = [754, 825, 1362, 1487, 2001, 2653];
+const restaurantDistMatrix = [
+  [0, 814, 1828, 2242, 2784, 3615],
+  [680, 0, 1120, 2061, 2269, 3100],
+  [1633, 972, 0, 2658, 1814, 3067],
+  [2242, 2173, 2403, 0, 1982, 1262],
+  [2799, 2313, 1342, 1610, 0, 1085],
+  [3345, 3148, 2176, 1262, 1085, 0],
 ];
 
 function findDistance(point1, point2) {
@@ -24,11 +20,11 @@ function findDistance(point1, point2) {
 }
 
 function getInputPoints() {
-  let points = REST_POS;
+  let noOfRestaurants = distFromStart.length;
   let newData = [];
-  for (let i = 0; i < points.length; i++) {
-    for (let j = i + 1; j < points.length; j++) {
-      let edge = [i, j, MAX_DISTANCE - findDistance(points[i], points[j])];
+  for (let i = 0; i < noOfRestaurants; i++) {
+    for (let j = i + 1; j < noOfRestaurants; j++) {
+      let edge = [i, j, MAX_DISTANCE - restaurantDistMatrix[i][j]];
       newData.push(edge);
     }
   }
@@ -44,8 +40,8 @@ function findOptimalMatching() {
 
 function distanceFromOrigin() {
   let totalDistance = 0;
-  REST_POS.forEach((position) => {
-    totalDistance = totalDistance + findDistance(FOOD_BANK_POS, position);
+  distFromStart.forEach((distance) => {
+    totalDistance = totalDistance + distance;
   });
   return totalDistance;
 }
@@ -54,8 +50,7 @@ function distanceForOptimalMatch() {
   let optimalMatch = findOptimalMatching();
   let totalDistance = 0;
   for (let i = 0; i < optimalMatch.length; i++) {
-    totalDistance =
-      totalDistance + findDistance(REST_POS[i], REST_POS[optimalMatch[i]]);
+    totalDistance = totalDistance + restaurantDistMatrix[i][optimalMatch[i]];
   }
   return totalDistance / 2;
 }
